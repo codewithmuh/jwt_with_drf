48a6d5625bbbcb979667a004a6c09d3661ca3b4d
Muhammad Rashid <rashiddaha80@gmail.com>
2022-08-17 16:07:42 +0500

    new changes

--- notes/views.py
+++ notes/views.py
@@ -2,7 +2,7 @@
 # notes/views.py
 from rest_framework import viewsets
 from .models import Notes
-from serializers import NoteSerializer
+from .serializers import NoteSerializer
 
 class NoteViewSet(viewsets.ModelViewSet):
     notes = Notes.objects.all()
